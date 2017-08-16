399e6045e96dcd4bf358eb404a954dca39d7ed3e
Naveed Ahmad <sr.naveed99@gmail.com>
2017-08-16 22:23:52 +0500

    Initial commit

---
+++ app/src/main/java/com/webservicegetrequestandroid/MainActivity.java
@@ -0,0 +1,44 @@
+package com.webservicegetrequestandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = this.getClass().getSimpleName();
+    private final String URL = "http://34.212.14.92:8089/adservices/studio.php?operation=GetPremiumApp";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void callWebServcie(View view) {
+
+        AsyncHttpClient asyncHttpClient=new AsyncHttpClient();
+        asyncHttpClient.get(URL, null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+                String jsonResponse = response.toString();
+                Log.i(TAG, "onSuccess: " + jsonResponse);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+                Log.e(TAG, "onFailure: " + errorResponse);
+            }
+        });
+    }
+}
